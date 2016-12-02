36dc416a978e80b1f08037a9a2d15e14b6baeed4
Josh Han <jhan@adacado.com>
2016-12-02 13:33:57 -0800

    day2

--- day1.js
+++ day1.js
@@ -1,6 +1,6 @@
 var question = "L5, R1, R4, L5, L4, R3, R1, L1, R4, R5, L1, L3, R4, L2, L4, R2, L4, L1, R3, R1, R1, L1, R1, L5, R5, R2, L5, R2, R1, L2, L4, L4, R191, R2, R5, R1, L1, L2, R5, L2, L3, R4, L1, L1, R1, R50, L1, R1, R76, R5, R4, R2, L5, L3, L5, R2, R1, L1, R2, L3, R4, R2, L1, L1, R4, L1, L1, R185, R1, L5, L4, L5, L3, R2, R3, R1, L5, R1, L3, L2, L2, R5, L1, L1, L3, R1, R4, L2, L1, L1, L3, L4, R5, L2, R3, R5, R1, L4, R5, L3, R3, R3, R1, R1, R5, R2, L2, R5, L5, L4, R4, R3, R5, R1, L3, R1, L2, L2, R3, R4, L1, R4, L1, R4, R3, L1, L4, L1, L5, L2, R2, L1, R1, L5, L3, R4, L1, R5, L5, L5, L1, L3, R1, R5, L2, L4, L5, L1, L1, L2, R5, R5, L4, R3, L2, L1, L3, L4, L5, L5, L2, R4, R3, L5, R4, R2, R1, L5";
 
-function level1(str) {
+function day1(str) {
 
     let aLeft = 0;
     let aUp = 0;
@@ -52,13 +52,13 @@ function level1(str) {
 
 const assert = require('assert');
 
-describe('level1', ()=>{
+describe('day1', ()=>{
     it('should return 2 when input is \'R2, R2, R2\'', ()=>{
-        assert.equal(level1('R2, R2, R2'), 2);
+        assert.equal(day1('R2, R2, R2'), 2);
     });
     it('should return 12 when input is \'R5, L5, R5, R3\'', ()=>{
-        assert.equal(level1('R5, L5, R5, R3'), 12);
+        assert.equal(day1('R5, L5, R5, R3'), 12);
     });
 })
 
-console.log("The result is: " + level1(question));
+console.log("The result is: " + day1(question));
