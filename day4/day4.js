80730da2623067c0c7b66d1001dc177bd4c5ca3c
Josh Han <jhan@adacado.com>
2016-12-08 15:57:10 -0800

    day4

--- day4/day4.js
+++ day4/day4.js
@@ -1,5 +1,23 @@
 const fs = require('fs');
 const file = fs.readFileSync('data', 'utf8');
+// const file = `aaaaa-bbb-z-y-x-123[abxyz]
+// a-b-c-d-e-f-g-h-987[abcde]
+// not-a-real-room-404[oarel]
+// totally-real-room-200[decoy]`
+
+function part1() {
+    const inputs = file.split('\n');
+    // inputs.pop();
+    const rooms = inputs.filter(input=>input).map(input=>extractData(input));
+    let sum = 0;
+    rooms.forEach((room, index)=>{
+        if (isRealRoom(room)) {
+            sum += room.id;
+            console.log(inputs[index]);
+        }
+    })
+    console.log(sum);
+}
 
 function extractData(str) {
     const data = str.split('-');
@@ -10,14 +28,34 @@ function extractData(str) {
 }
 
 function isRealRoom(room) {
-    return false;
+    let charset = {};
+    let chars = room.data.join('').split('');
+    chars.forEach(char => {
+        charset[char] = charset[char] || 0;
+        charset[char] += 1;
+    })
+    let array =
+        Object.keys(charset)
+        .map(key=>{
+            return {key: key, value: charset[key]};
+        }).sort((a, b) => {
+            if (a.value == b.value) {
+                return a.key > b.key;
+            } else {
+                return a.value < b.value;
+            }
+        });
+    let toCheck = '';
+    for (let i = 0; i < 5; i++) {
+        toCheck += array[i].key;
+    }
+    return toCheck == room.checksum;
 }
 
-function part1() {
-    const inputs = file.split('\n');
-    inputs.pop();
-    b = inputs.map(input=>extractData(input));
-    console.log(b);
-}
+// console.log(isRealRoom(extractData('aaaaa-bbb-z-y-x-123[abxyz]')));
+// console.log(isRealRoom(extractData('a-b-c-d-e-f-g-h-987[abcde]')));
+// console.log(isRealRoom(extractData('not-a-real-room-404[oarel]')));
+// console.log(isRealRoom(extractData('totally-real-room-200[decoy]')));
+
 
 part1();
