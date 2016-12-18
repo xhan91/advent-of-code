15c4b0bd45440bf062594e48c4a8fb04a3e2bc8f
Josh Han <xhan91ca@gmail.com>
2016-12-18 16:52:28 +0800

    day13

--- day13/day13.js
+++ day13/day13.js
@@ -22,18 +22,47 @@ function drawTheMap(puzzleInput) {
     return map;
 }
 
-function findThePath(map, x, y) {
+function findThePath(_map) {
     let found = false;
     let step = 0;
     let grey = [ { x: 1, y: 1 } ];
-    while (!found) {
-        grey.forEach(pos=>{
-            let { x, y } = pos;
-            
-        })
-    }
-    let copy = JSON.parse(JSON.stringify(map));
+    let map = _map;
+    map[1][1] = 'O';
 
+    while (!found) {
+        let copy = JSON.parse(JSON.stringify(map));
+        let newGrey = [];
+        step += 1;
+        grey.forEach(pos => {
+            if (found) return;
+            let {x, y} = pos;
+            f1 = checkAndPushToGrey(map, copy, x+1, y, newGrey);
+            f2 = checkAndPushToGrey(map, copy, x-1, y, newGrey);
+            f3 = checkAndPushToGrey(map, copy, x, y+1, newGrey);
+            f4 = checkAndPushToGrey(map, copy, x, y-1, newGrey);
+            found = f1 || f2 || f3 || f4;
+        });
+        map = copy;
+        grey = newGrey;
+    }
+
+    console.log(step);
+}
+
+function checkAndPushToGrey(map, copy, x, y, grey) {
+    if (checkPoint(map, x, y)) {
+        copy[x][y] = 'O';
+        grey.push({x: x, y: y});
+    }
+    // This is checking if the des is found, should be passed value instead of hardcoding.
+    return (x === 31 && y === 39 );
+}
+
+function checkPoint(map, x, y) {
+    if (x >= 0 && y >= 0) {
+        return map[x][y] === '.';
+    }
+    return false;
 }
 
 // For visualization
@@ -49,3 +78,4 @@ function printTheMap(map, width, height) {
 // the test data
 // const map = drawTheMap(10);
 // printTheMap(map, 10, 7);
+findThePath(drawTheMap(1362));
