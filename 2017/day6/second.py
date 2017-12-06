5db90b72ac1477b860facd509b19ab3d7e61e457
Josh Han <jhan@adacado.com>
2017-12-05 21:51:35 -0800

    2017 day6 solved

---
+++ 2017/day6/second.py
@@ -0,0 +1,35 @@
+
+
+def get_max_index_value(arr):
+    max_v = max(arr)
+    index = arr.index(max_v)
+    return index, max_v
+
+
+def spread(arr):
+    length = len(arr)
+    index, max_v = get_max_index_value(arr)
+    arr[index] = 0
+    for i in range(max_v):
+        index += 1
+        index = index % length
+        arr[index] += 1
+
+
+with open('input', 'r') as input:
+    for line in input:
+        arr = map(int, line.strip().split('\t'))
+
+# arr = [0,2,7,0]
+
+key = ','.join(map(str, arr))
+book = {}
+sum_v = 0
+
+while key not in book.keys():
+    book[key] = sum_v
+    sum_v += 1
+    spread(arr)
+    key = ','.join(map(str, arr))
+
+print sum_v - book[key]
