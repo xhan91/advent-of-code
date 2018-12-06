d52e09ba0ec36a433f9e5b7c3da1639c1434305e
Josh Han <xhan91ca@gmail.com>
2018-12-05 17:00:00 -0800

    wip

--- 2018/day04/first.py
+++ 2018/day04/first.py
@@ -1,11 +1,15 @@
+import datetime
+
 date_guard = {}
 date_timetable = {}
 guard_time = {}
 
 def processLine(s, date_guard, date_timetable):
   dateTime, action = s.split('] ')
-  date, time = dateTime[6:].split(' ')
-  minite = int(time.split(':')[1])
+  date, time = dateTime[1:].split(' ')
+  hour, minite = map(int, time.split(':'))
+  if hour != 0:
+    date = (datetime.datetime.strptime(date,'%Y-%m-%d') + datetime.timedelta(days=1)).strftime('%Y-%m-%d')
   if action[0] == 'G':
     guard = action.split(' ')[1][1:]
     date_guard[date] = guard
@@ -16,7 +20,7 @@ def processLine(s, date_guard, date_timetable):
       date_timetable[date] = [''] * 60
     else:
       date_timetable[date][minite] = 'w'
-  elif action[0] == 'f':
+  elif action[0] == 'f':      
     if date not in date_timetable.keys():
       date_timetable[date] = [''] * 60
     else:
@@ -39,10 +43,7 @@ for timetable in date_timetable.values():
 #   print(d, date_guard[d], ''.join(date_timetable[d]))
 
 for date, timetable in date_timetable.items():
-  if date in date_guard.keys():
-    guard = date_guard[date]
-  else:
-    continue
+  guard = date_guard[date]
   if guard not in guard_time.keys():
     guard_time[guard] = 0
   for i in range(60):
@@ -54,12 +55,6 @@ for k,v in guard_time.items():
   if v == the_time:
     the_guard = k
 
-print(the_guard)
-
-for d in date_guard.keys():
-  if date_guard[d] == '3571':
-    print(d, date_guard[d], ''.join(date_timetable[d]))
-
 all_timetable = [0] * 60
 for date, guard in date_guard.items():
   if guard == the_guard:
@@ -68,13 +63,21 @@ for date, guard in date_guard.items():
       if timetable[i] == '#':
         all_timetable[i] += 1
 
-tmp = list(map(str, all_timetable))
-print(''.join(tmp))
+tmp = list(map(lambda x: '0' + x if len(x) == 1 else x, map(str, range(60))))
+print(' ' * 16 + ''.join(map(lambda x: x[0], tmp)))
+print(' ' * 16 + ''.join(map(lambda x: x[1], tmp)))
+for d in date_guard.keys():
+  if date_guard[d] == '2287':
+    print(d, date_guard[d], ''.join(date_timetable[d]))
+tmp = list(map(lambda x: '0' + x if len(x) == 1 else x, map(str, all_timetable)))
+print(' ' * 16 + ''.join(map(lambda x: x[0], tmp)))
+print(' ' * 16 + ''.join(map(lambda x: x[1], tmp)))
 
 max_minute = max(all_timetable)
 for i in range(60):
   if all_timetable[i] == max_minute:
     the_minute = i
-    break
 
+print(the_guard)
+print(the_minute)
 print(int(the_guard) * the_minute)
