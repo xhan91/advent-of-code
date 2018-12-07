6c67c800ecdd75a3a8e332e883d0e65857f99e42
Josh Han <xhan91ca@gmail.com>
2018-12-07 14:30:31 -0800

    201807 wip

--- 2018/day07/first.py
+++ 2018/day07/first.py
@@ -1,24 +1,30 @@
 import string
 
 char_req = {}
-appered = {}
+appeared = {}
 done = {}
 result = ''
 
 with open('input.txt', 'r') as input:
   for line in input:
     ent, pre = line[36], line[5]
-    appered[ent] = True
-    appered[pre] = True
+    appeared[ent] = True
+    appeared[pre] = True
     try:
       char_req[ent].append(pre)
     except:
       char_req[ent] = [pre]
 
-appered = list(appered.keys())
-appered.sort()
+appeared = list(appeared.keys())
+appeared.sort()
 
-for c in appered:
+for c in appeared:
+  try:
+    print(c, char_req[c])
+  except:
+    print(c)
+
+for c in appeared:
   if c not in char_req.keys():
     done[c] = True
     result += c
@@ -27,13 +33,14 @@ for c in appered:
 
 guard = True
 while guard:
-  for c in appered:
+  for c in appeared:
     try:
       reqs = char_req[c]
       canMove = True
       for req in reqs:
         if not done[req]:
           canMove = False
+          break
       if canMove:
         result += c
         done[c] = True
